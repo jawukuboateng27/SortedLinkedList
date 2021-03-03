019d1a23175d543194afe3fae58b415218290f56
Faisal Hossain <faisal.hossain75@gmail.com>
2021-03-02 23:18:24 -0500

    Add comments

--- ItemType.h
+++ ItemType.h
@@ -1,15 +1,25 @@
-
+#ifndef ITEMTYPE_H
+#define ITEMTYPE_H
 
 enum Comparison{GREATER,LESS,EQUAL};
 
- class ItemType
- {
- public:
-    ItemType();
-    Comparison compareTo(ItemType item);
-    int getValue()const;
-    void initialize(int num);
+class ItemType {
+   
+   // Public Variables
+   public:
+      // Default constructor
+      ItemType();
+      // Value Comparison Method
+      Comparison compareTo(ItemType item);
+      // Value Getter Method
+      int getValue()const;
+      // Variable Initializing Method
+      void initialize(int num);
+   
+   // Private Variables
+   private:
+      // Value
+      int value;
+};
 
- private:
-    int value;
- };
+#endif

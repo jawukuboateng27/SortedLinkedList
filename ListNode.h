d5a88420afb1d9def49269ed5244e2c82ac634f9
Faisal Hossain <faisal.hossain75@gmail.com>
2021-03-02 23:21:06 -0500

    Add comments

--- ListNode.h
+++ ListNode.h
@@ -1,7 +1,15 @@
+#ifndef LISTNODE_H
+#define LISTNODE_H
 #include "ItemType.h"
 
-    struct ListNode
-    {
-            ItemType item;
-            ListNode *next;
-    };
+struct ListNode {
+
+   // Public Variables
+   public:
+        // Item Object
+        ItemType item;
+        // Next Pointer
+        ListNode *next;
+};
+
+#endif

fc979d451171cdad18615eee68f4e66c58595b3b
Faisal Hossain <faisal.hossain75@gmail.com>
2021-03-02 23:18:09 -0500

    Add constructors

--- ItemType.cpp
+++ ItemType.cpp
@@ -1,23 +1,39 @@
 #include<iostream>
 #include "ItemType.h"
 
-    Comparison ItemType::compareTo(ItemType item)
-    {
-          if(value < item.value){
-                return LESS;
-          }else if (value > item.value){
-                return GREATER;
-          }else
-              return EQUAL;
-    }
+// Default Constructor
+ItemType::ItemType() {
+    value = 0;
+}
 
-    int ItemType::getValue() const
-    {
-        return value;
-    }
+// Constructor
+ItemType::ItemType(int value) {
+    this->value = value;
+}
 
-    void ItemType::initialize(int num)
-    {
-        this->value = num;
+// Value Comparison Method
+Comparison ItemType::compareTo(ItemType item) {
+    // Parameter value is less than the current object's value
+    if (value < item.value) {
+        return LESS;
     }
+    // Parameter value is greater than the current object's value
+    else if (value > item.value) {
+        return GREATER;
+    }
+    // Parameter value is equal to the current object's value
+    else {
+        return EQUAL;
+    }
+}
+    
+// Return the value instance variable
+int ItemType::getValue() const {
+    return value;
+}
+
+//  Initializes the data member by variable num
+void ItemType::initialize(int num) {
+    this->value = num;
+}
 
